100e3bb49c4f1aabdd0914717f741b59a8b2b9b8
Ryan Adi Putra <ryannadiputraa@gmail.com>
2023-04-11 14:25:28 +0800

    chore: init redis

---
+++ pkg/cache/redis.go
@@ -0,0 +1,49 @@
+package cache
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/spf13/viper"
+)
+
+type redisClient struct {
+	rdb *redis.Client
+}
+
+type Redis interface {
+	Set(ctx context.Context, key string, value interface{}) error
+	Get(ctx context.Context, key string) (string, error)
+}
+
+func InitRedis() Redis {
+	opt, err := redis.ParseURL(viper.GetString("REDIS_URL"))
+	if err != nil {
+		panic(fmt.Sprintf("fail to connect to redis: %s", err))
+	}
+
+	rdb := redis.NewClient(opt)
+	return &redisClient{rdb: rdb}
+}
+
+func (r *redisClient) Set(ctx context.Context, key string, value interface{}) error {
+	jsonData, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+
+	err = r.rdb.Set(ctx, key, jsonData, 10*time.Minute).Err()
+	return err
+}
+
+func (r *redisClient) Get(ctx context.Context, key string) (string, error) {
+	val, err := r.rdb.Get(ctx, key).Result()
+	if err != nil {
+		return "", err
+	}
+
+	return val, err
+}
