fd4422616909b28c9d834ec4011d3d8bc7c4225a
Ryan Adi Putra <ryannadiputraa@gmail.com>
2023-05-01 11:05:47 +0800

    fix: category order

--- app/product/repository/product_repository.go
+++ app/product/repository/product_repository.go
@@ -76,7 +76,7 @@ func (r *productRepository) FetchCategory(ctx context.Context) ([]domain.Product
 	var categories []domain.ProductCategory
 	cache, err := r.redis.Get(ctx, "products:category")
 	if err != nil {
-		err := r.db.Find(&categories).Error
+		err := r.db.Order("category").Find(&categories).Error
 		if err != nil {
 			return nil, err
 		}
